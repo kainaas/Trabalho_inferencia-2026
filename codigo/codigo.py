03937b5254015208d94ede726f98e5792ee0c838
Kainas <165678304+kainaas@users.noreply.github.com>
2026-04-09 17:34:53 -0300

    plot of the qualitative variable done

--- codigo/codigo.py
+++ codigo/codigo.py
@@ -2,14 +2,49 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-file_name = "../penguins.csv"
-df = pd.read_csv(file_name)
+file_in = "../penguins.csv"
+folder_out = "../plots/"
+plot_quali = folder_out + "plots_quali.pdf"
 
-print(df.dtypes)
+df = pd.read_csv(file_in)
 
-species_quali = df['species']
-species_quali_freq = pd.DataFrame(species_quali.value_counts())
-species_quali.value_counts().plot.pie(y = 'count')
+#print(df.dtypes)
+
+#Treating the qualitative data "species", which has the species of the studied penguins
+species = df['species'].value_counts().reset_index()
+species.columns = ['species', 'frequency']
+print(species)
+
+
+#Plots for the qualitative variable
+fig, axs = plt.subplots(1, 2, figsize=(12, 5))
+
+fig.suptitle('Distribuição das Espécies de Pinguins', fontsize=14)
+
+# Pie chart
+axs[0].pie(species['frequency'], labels=species['species'], autopct='%1.1f%%')
+axs[0].set_title('Distribuição (Pizza)')
+
+# bars
+bars = axs[1].bar(species['species'], species['frequency'])
+axs[1].set_title('Frequência (Barras)')
+axs[1].set_xlabel('Espécies')
+axs[1].set_ylabel('Frequência')
+for bar in bars:
+    height = bar.get_height()
+    axs[1].text(
+        bar.get_x() + bar.get_width() / 2,
+        height,
+        f'{int(height)}',
+        ha='center',
+        va='bottom'
+    )
+
+fig.savefig(plot_quali)
 plt.show()
+
+
+
+
 #prints a lot of information (like mean, min, max, percentile...)
 #print(df.describe())
