82c8fc8befad52c11cb4dca9b7cd528e0f2e55cb
Kainas <165678304+kainaas@users.noreply.github.com>
2026-04-10 19:18:50 -0300

    changed dataset, qualitative variable now has a table!

--- codigo/qualitativa.py
+++ codigo/qualitativa.py
@@ -1,19 +1,19 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
-file_in = "../penguins.csv"
+file_in = "../cars_data.csv"
 folder_out = "../plots/"
 plot_quali = folder_out + "plots_quali.pdf"
+table_quali = folder_out + "table_quali"
 
 df = pd.read_csv(file_in)
 
 #print(df.dtypes)
 
-#Treating the qualitative data "species", which has the species of the studied penguins
-species = df['species'].value_counts().reset_index()
-species.columns = ['species', 'frequency']
-print(species)
+#Treating the qualitative data "origin", which has the origin of the studied penguins
+origin = df['origin'].value_counts().reset_index()
+origin.columns = ['origin', 'frequency']
+print(origin)
 
 
 #Plots for the qualitative variable
@@ -22,13 +22,13 @@ fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 fig.suptitle('Distribuição das Espécies de Pinguins', fontsize=14)
 
 # Pie chart
-axs[0].pie(species['frequency'], labels=species['species'], autopct='%1.1f%%')
+axs[0].pie(origin['frequency'], labels=origin['origin'], autopct='%1.1f%%')
 axs[0].set_title('Distribuição (Pizza)')
 
 # bars
-bars = axs[1].bar(species['species'], species['frequency'])
+bars = axs[1].bar(origin['origin'], origin['frequency'])
 axs[1].set_title('Frequência (Barras)')
-axs[1].set_xlabel('Espécies')
+axs[1].set_xlabel('Origem')
 axs[1].set_ylabel('Frequência')
 for bar in bars:
     height = bar.get_height()
@@ -41,7 +41,10 @@ for bar in bars:
     )
 
 fig.savefig(plot_quali)
-plt.show()
+#plt.show()
+
+origin.to_latex(table_quali)
+
 
 
 
